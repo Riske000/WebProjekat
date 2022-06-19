da7896082db232e3e3d4cd3b89625482cbd150f0
Riske000 <nemanjariske@gmail.com>
2022-06-19 18:09:59 +0200

    added login and fixed registration

--- WebShopREST/WebContent/js/login.js
+++ WebShopREST/WebContent/js/login.js
@@ -1,3 +1,27 @@
-/**
- * 
- */
+var app = new Vue({
+	el: '#loginForm',
+	data: {
+		userToLogin: {},
+		error: ''
+	},
+	mounted() {
+		this.userToLogin = {
+			intId: '', korisnickoIme: null, sifra: null
+		}
+	},
+	methods: {
+		loginUser : function(event) {
+			this.error = ""
+			if (!this.userToLogin.korisnickoIme || !this.userToLogin.sifra) {
+				this.error = "Sva polja moraju biti uneta!";
+				event.preventDefault();
+				return;
+			}
+			axios.post('rest/korisnik1/login', this.userToLogin)
+				.then((response) => {
+					alert('Uspesno ste se ulogovali')
+				})
+			event.preventDefault();
+		}
+	}
+});
--- WebShopREST/src/services/KorisnikService1.java
+++ WebShopREST/src/services/KorisnikService1.java
@@ -2,15 +2,14 @@ package services;
 
 import javax.annotation.PostConstruct;
 import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
-
-
-
+import javax.ws.rs.core.Response;
 
 import beans.Korisnik;
 import dao.KorisnikDAO;
@@ -48,4 +47,17 @@ public class KorisnikService1 {
 		
 	}
 
+	@POST
+	@Path("/login")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response login(Korisnik korisnik, @Context HttpServletRequest request) {
+		KorisnikDAO korisnikDao = (KorisnikDAO) ctx.getAttribute("korisnikDAO");
+		Korisnik logovaniKorisnik = korisnikDao.checkKorisnickoImeSifra(korisnik.getKorisnickoIme(), korisnik.getSifra());
+		if (logovaniKorisnik == null) {
+			return Response.status(400).entity("Invalid username and/or password").build();
+		}
+		request.getSession().setAttribute("user", logovaniKorisnik);
+		return Response.status(200).build();
+	}
 }
